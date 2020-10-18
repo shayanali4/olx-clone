28ec5494afbc9f5c2820e9361cf9838d8220662d
shayanali4 <shayana803@gmail.com>
2020-10-18 13:25:15 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <>
-        
-      </>
+      <h1>
+        OLX App
+      <h1/>
     )
   }
 }
