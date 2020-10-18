8c0ee8a73dda50e45939079478df29519e623db4
shayanali4 <shayana803@gmail.com>
2020-10-18 13:21:34 +0500

    olx App

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+<h1>OLX App</h1>        </a>
       </header>
     </div>
   );
